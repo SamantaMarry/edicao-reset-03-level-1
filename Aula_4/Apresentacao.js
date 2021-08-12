f9dfaf6e8ccc5260a6274e38e796f91e19525a61
Samanta Marry <samantamaarry@gmail.com>
2021-08-12 17:52:24 -0300

    Meu primeiro commit

---
+++ Aula_4/Apresentacao.js
@@ -0,0 +1,13 @@
+nomeCompleto = "Samanta Marry da Silva Britto";
+apelido = 'Samy';
+idade = 28;
+dataNascimento = "18 de outubro de 1992";
+localNascimento = 'Porto Alegre/RS';
+altura = 165;
+trabalhando = true
+
+apresentacao = 'Meu nome é ' + nomeCompleto + '(sou conhecida como: ' + apelido + ') e tenho ' + idade + ' anos. Nasci no dia ' + dataNascimento + ', na cidade de ' + localNascimento + '. Tenho ' + altura + ' de altura e atualmente estou ' + (trabalhando ? 'empregado' : 'desempregado') + '.'
+
+console.log(apresentacao);
+
+
