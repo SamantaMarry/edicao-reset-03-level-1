35a26e19b9c5a25e0c143b53ec4a2836c80187a0
Samanta Marry <samantamaarry@gmail.com>
2021-08-13 16:36:56 -0300

    novo exercicio

--- Aula_5/Operadores/exercicio.js
+++ Aula_5/Operadores/exercicio.js
@@ -1,7 +1,59 @@
-a = 14
-b = 20
+// Mesmo Nome
 
-a++
+nomeFulado = 'samanta'
+nomeBeltrano = 'iury'
 
-console.log(a)
+mesmoNome = (nomeFulado == nomeBeltrano)
 
+console.log(mesmoNome)
+
+
+// Maior de Idade
+
+idade = 15
+
+maiorDeIdade = (idade >= 18)
+
+console.log(maiorDeIdade)
+
+// valor com juros
+
+boleto = 100.00
+
+valorComJuros = (boleto * 1.1)
+
+console.log(valorComJuros)
+
+// media aritmética
+
+item1 = 5
+item2 = 6
+item3 = 7
+item4 = 5
+item5 = 6
+
+mediaAritmetica = ( item1 + item2 + item3 + item4 + item5 ) / 5
+
+console.log(mediaAritmetica)
+
+// margem bruta
+
+receitaLiquida = 1500.00
+custoProdutos = 50.00
+
+lucroBruto = ( receitaLiquida - custoProdutos)
+
+margemBruta = ( lucroBruto / receitaLiquida) * 100
+
+console.log(margemBruta)
+
+// saldo da conta 
+
+saldo = 1000.00
+saldo = 1000.00 - 99.90
+saldo = saldo + 2500.00
+saldo = saldo - 0.10
+saldo = saldo * 0.75
+saldo = saldo / 2
+
+console.log(saldo)
