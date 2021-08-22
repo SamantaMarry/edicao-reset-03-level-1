f9676843f78848f9e3e86cb0cd2248dfd52f6a7d
Samanta Marry <samantamaarry@gmail.com>
2021-08-22 00:50:02 -0300

    Nova Aula

---
+++ Modulo_2/Aula_2/Exercicio/AlunoResetComParametro.java
@@ -0,0 +1,7 @@
+class AlunoResetComParametro {
+
+    public static void main(String[] args) {
+       
+       System.out.println("Sou aluno do Level 1 do CWI Reset! Me chamo " + args[0]);
+   }
+}
