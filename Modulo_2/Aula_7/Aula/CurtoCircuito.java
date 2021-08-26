3731306f0cfbea247fd41eeeed98869bb166b0a1
Samanta Marry <samantamaarry@gmail.com>
2021-08-25 21:18:02 -0300

    Nova Aula

---
+++ Modulo_2/Aula_7/Aula/CurtoCircuito.java
@@ -0,0 +1,21 @@
+package Aula;
+
+public class CurtoCircuito {
+
+    public static void main(String[] args) {
+
+        boolean verdadeiro = true;
+        boolean falso = false;
+        boolean resultado1 = falso & 
+                verdadeiro;
+        boolean resultado2 = falso && 
+                verdadeiro;
+        System.out.println(resultado1);
+        System.out.println(resultado2);
+    
+    
+
+
+    }
+    
+}
---
+++ Modulo_2/Aula_7/Aula/OperadoresAritmeticos.java
@@ -0,0 +1,79 @@
+package Aula;
+
+public class OperadoresAritmeticos{
+
+    public static void main(String[] args) {
+        
+
+        int resultado = 1 + 2;
+        System.out.println(resultado);
+
+        resultado = resultado - 1;
+        System.out.println(resultado);
+
+        resultado = resultado * 2;
+        System.out.println(resultado);
+
+        resultado = resultado / 2;
+        System.out.println(resultado);
+
+        resultado = resultado + 8;
+        System.out.println(resultado);
+
+        resultado = resultado % 7;
+        System.out.println(resultado);
+
+
+        String primeiroNome = "Samanta";
+        String segundoNome = " Marry";
+        String terceiroNome = primeiroNome + segundoNome;
+        System.out.println(terceiroNome);
+
+        resultado = resultado + 1;
+        System.out.println(resultado);
+
+        resultado++;
+        System.out.println(resultado);
+
+        //5
+        System.out.println(resultado++);
+        //mesma coisa que:
+        //System.out.println(resultado);
+        //resultado = resultado + 1;
+        //resultado += 1;
+        
+        System.out.println(++resultado);
+        //mesma coisa que:
+        //resultado = resultado + 1;
+        //resultado += 1;
+        //System.out.println(resultado);
+
+        resultado--;
+        System.out.println(resultado);
+
+        //5
+        System.out.println(resultado--);
+        //mesma coisa que:
+        //System.out.println(resultado);
+        //resultado = resultado - 1;
+        //resultado -= 1;
+        
+        System.out.println(--resultado);
+        //mesma coisa que:
+        //resultado = resultado - 1;
+        //resultado -= 1;
+        //System.out.println(resultado);
+
+       
+
+
+
+
+
+
+    
+        
+    }
+
+}
+
